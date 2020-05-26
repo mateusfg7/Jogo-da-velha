2c162a2599c56e4324d745e168115702969415b4
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-26 11:12:13 -0300

    removing color variables

--- functions/rules.py
+++ functions/rules.py
@@ -1,6 +1,3 @@
-from functions.colors import cor
-cor = cor()
-
 def drawCondition(positions):
     markedPositions = 0
     index = 1
@@ -9,18 +6,22 @@ def drawCondition(positions):
             markedPositions += 1
         index += 1
     if markedPositions == 9:
-        print(f"{cor['white']}O jogo deu {cor['red']}empate{cor['white']}!")
+        print(f"\033[00;37mO jogo deu \033[00;31mempate\033[00;37m!")
         exit()
     else:
         return False
 
+
 def winCondition(one, two, three, position, player1, player2):
     if position[one] == player1 and position[two] == player1 and position[three] == player1:
-        print(f"\n{cor['white']}Parabens {cor['red']}{player1} {cor['white']}você ganhou!")
+        print(
+            f"\n\033[00;37mParabens \033[00;31m{player1} \033[00;37mvocê ganhou!")
         exit()
     if position[one] == player2 and position[two] == player2 and position[three] == player2:
-        print(f"\n{cor['white']}Parabens {cor['red']}{player2} {cor['white']}você ganhou!")
+        print(
+            f"\n\033[00;37mParabens \033[00;31m{player2} \033[00;37mvocê ganhou!")
         exit()
+
 
 def winCases():
     casosDeVitorias = [
@@ -35,6 +36,7 @@ def winCases():
     ]
 
     return casosDeVitorias
+
 
 def validateChoice(player, choice, position):
     if position[choice] == 'X' or position[choice] == 'O':
