3ff9133ae61852a343366a8ecc19c5b6ed6063d3
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-26 10:44:51 -0300

    expanding rules imports

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -1,18 +1,22 @@
-from functions.rules import *
+from functions.rules import drawCondition
+from functions.rules import validateChoice
+from functions.rules import winCases
+from functions.rules import winCondition
+
 from functions.render import render
 
 casosDeVitorias = winCases()
 
 position = {
-    '1' : '\033[00;37m1',
-    '2' : '\033[00;37m2',
-    '3' : '\033[00;37m3',
-    '4' : '\033[00;37m4',
-    '5' : '\033[00;37m5',
-    '6' : '\033[00;37m6',
-    '7' : '\033[00;37m7',
-    '8' : '\033[00;37m8',
-    '9' : '\033[00;37m9',
+    '1': '\033[00;37m1',
+    '2': '\033[00;37m2',
+    '3': '\033[00;37m3',
+    '4': '\033[00;37m4',
+    '5': '\033[00;37m5',
+    '6': '\033[00;37m6',
+    '7': '\033[00;37m7',
+    '8': '\033[00;37m8',
+    '9': '\033[00;37m9',
 }
 
 drawCondition(position)
@@ -21,7 +25,7 @@ currenctPlayer = 'X'
 
 while True:
     render(currenctPlayer, position)
-    
+
     for c in casosDeVitorias:
         winCondition(c[0], c[1], c[2], position, 'X', 'O')
     drawCondition(position)
@@ -29,13 +33,10 @@ while True:
     choice = input("\n-> ")
 
     if validateChoice(currenctPlayer, choice, position):
-        
+
         position[choice] = currenctPlayer
-        
+
         if currenctPlayer == 'X':
             currenctPlayer = 'O'
         elif currenctPlayer == 'O':
             currenctPlayer = 'X'
-
-    
-    
